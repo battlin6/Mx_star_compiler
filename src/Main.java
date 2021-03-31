6a94669ccc0fdbe9945be8d8313e8f09c4d3da82
battlin <battlin6@gmail.com>
2021-04-01 04:33:58 +0800

    I think I have found the reason

--- src/Main.java
+++ src/Main.java
@@ -14,11 +14,11 @@ import IR.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		ErrorReminder errorReminder = new ErrorReminder();
-		//InputStream IS = System.in;
-		InputStream IS = new FileInputStream("code.mx");
+		InputStream IS = System.in;
+		//InputStream IS = new FileInputStream("code.mx");
 		CharStream AIS = CharStreams.fromStream(IS);
       	
-		System.err.println("lexer------------------");
+		//System.err.println("lexer------------------");
 		MxstarLexer lexer = new MxstarLexer(AIS);
 		lexer.removeErrorListeners();
 		lexer.addErrorListener(new MxstarErrorListener(errorReminder));
@@ -31,29 +31,29 @@ public class Main {
 			System.out.println(token.getType() + " " + token.getText());
 		}*/
 		
-		System.err.println("parser------------------");
+		//System.err.println("parser------------------");
 		MxstarParser parser = new MxstarParser(tokens);
 		parser.removeErrorListeners();
 		parser.addErrorListener(new MxstarErrorListener(errorReminder));
 		
-		System.err.println("Building AST------------");
+		//System.err.println("Building AST------------");
 		ASTBuilder ast = new ASTBuilder(errorReminder);
 		ProgramNode root = (ProgramNode) ast.visit(parser.program());
 		
-		System.err.println("Semantic checking--------");
+		//System.err.println("Semantic checking--------");
 		SemanticChecker checker = new SemanticChecker(errorReminder);
 		checker.visit(root);
 		
 		if (errorReminder.count() > 0)
 			System.exit(errorReminder.count());
 		
-		System.err.println("Building IR--------------");
+		//System.err.println("Building IR--------------");
 		GlobalScope globalScope = checker.getGlobalScope();
 		StringType stringTemplate = checker.getStringTemplate();
 		IRBuilder ir = new IRBuilder(globalScope, stringTemplate, errorReminder);
 		ir.visit(root);
 		
-		System.err.println("Printing IR--------------");
+		//System.err.println("Printing IR--------------");
 		IRModule module = ir.getModule(); 
 		IRPrinter printer = new IRPrinter();
 		printer.visit(module);
