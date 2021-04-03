bbe0e7ea5339016966b99d8c6d8af7651edab219
battlin <battlin6@gmail.com>
2021-04-03 23:12:30 +0800

    add ravel add missed inner function

--- output.s
+++ output.s
@@ -1,21 +1,86 @@
 	.text
 
+	.globl  qpow
+	.p2align	2
+	.type   qpow, @function
+qpow:
+	sw      a0,0(a5)
+	sw      a1,0(a5)
+	sw      a2,0(a5)
+	li      a2,1
+	sw      a2,0(a5)
+	lw      a2,0(a5)
+	sw      a2,0(a5)
+	j       qpow_whileCondBlock.0
+
+qpow_whileCondBlock.0:
+	lw      a4,0(a5)
+	li      a2,0
+	bgt     a4,a2,qpow_whileBodyBlock.0
+	j       qpow_afterWhileBlock.0
+
+qpow_whileBodyBlock.0:
+	j       qpow_ifCondBlock.0
+
+qpow_afterWhileBlock.0:
+	lw      a2,0(a5)
+	sw      a2,0(a5)
+	j       qpow_returnBlock.0
+
+qpow_ifCondBlock.0:
+	lw      a2,0(a5)
+	andi    a2,a2,1
+	li      a4,1
+	beq     a2,a4,qpow_thenBodyBlock.0
+	j       qpow_afterIfBlock.0
+
+qpow_thenBodyBlock.0:
+	lw      a2,0(a5)
+	lw      a2,0(a5)
+	lw      a4,0(a5)
+	mul     a2,a2,a4
+	lw      a4,0(a5)
+	rem     a2,a2,a4
+	sw      a2,0(a5)
+	j       qpow_afterIfBlock.0
+
+qpow_afterIfBlock.0:
+	lw      a2,0(a5)
+	lw      a4,0(a5)
+	lw      a2,0(a5)
+	mul     a2,a4,a2
+	lw      a4,0(a5)
+	rem     a2,a2,a4
+	sw      a2,0(a5)
+	lw      a2,0(a5)
+	lw      a4,0(a5)
+	li      a2,2
+	div     a2,a4,a2
+	sw      a2,0(a5)
+	j       qpow_whileCondBlock.0
+
+qpow_returnBlock.0:
+	lw      a0,0(a5)
+	jr      ra
+
+
 	.globl  main
 	.p2align	2
 	.type   main, @function
 main:
 	addi    sp,sp,-16
 	sw      ra,12(sp)
-	mv      a6,s0
-	sw      a6,8(sp)
+	mv      a5,s0
+	sw      a5,8(sp)
 	call    __init__
-	lw      a6,0(s0)
-	addi    a6,a6,1
-	sw      a6,0(s0)
-	addi    a6,a6,1
-	sw      a6,0(s0)
-	li      a6,0
-	sw      a6,0(s0)
+	li      a0,2
+	li      a1,10
+	li      a2,10000
+	call    qpow
+	call    __toString
+	call    __println
+	li      a5,0
+	sw      a5,0(s0)
 	j       main_returnBlock.0
 
 main_returnBlock.0:
